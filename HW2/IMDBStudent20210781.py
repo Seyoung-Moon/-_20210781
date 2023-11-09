32d72e60fb7649f2462c94e3a5ee2191ab34d4c8
20210781 문세영 <102609564+Seyoung-Moon@users.noreply.github.com>
2023-11-09 23:42:15 +0900

    Update IMDBStudent20210781.py

--- HW2/IMDBStudent20210781.py
+++ HW2/IMDBStudent20210781.py
@@ -5,19 +5,20 @@ def count_movies_by_genre(input_file, output_file):
 
     with open(input_file, 'r', encoding='utf-8') as file:
         for line in file:
-            _, _, genres = line.strip().split('::')
-            genres = genres.split('|')
-            for genre in genres:
-                genre_count[genre] = genre_count.get(genre, 0) + 1
+            data = line.strip().split('::')
+            if len(data) >= 3:
+                _, _, genres = data
+                genres = genres.split('|')
+                for genre in genres:
+                    genre_count[genre] = genre_count.get(genre, 0) + 1
+            else:
+                print(f"Issue with line: {line}")
 
     with open(output_file, 'w', encoding='utf-8') as output:
         for genre, count in genre_count.items():
             output.write(f"{genre} {count}\n")
 
 if __name__ == '__main__':
-    if len(sys.argv) != 1:
-        input_file = input("Enter the input file name: ")
-        output_file = input("Enter the output file name: ")
-        count_movies_by_genre(input_file, output_file)
-    else:
-        print("Usage: python3 IMDBStudent20210781.py")
+    input_file = "movies_exp.txt"
+    output_file = "movieoutput.txt"
+    count_movies_by_genre(input_file, output_file)
